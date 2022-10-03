36b77c3bb5114d6fa90854932e06718c066439ab
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-02 22:58:17 -0400

    Time: 56 ms (16.67%), Space: 13.9 MB (66.67%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -2,8 +2,12 @@ class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         if a < b:
             return self.commonFactors(b, a)
+        
+        # Max Common Divider
         while b != 0:
             a, b = b, a % b
+        
+        # Get the number of factors of a
         factor2num = dict()
         factor = 2
         while a != 1:
