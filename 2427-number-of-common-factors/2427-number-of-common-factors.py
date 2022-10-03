cffa3daec29368e61d1ad24e853f0e67eeb0b8ff
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-02 22:56:20 -0400

    Time: 48 ms (33.33%), Space: 14 MB (66.67%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,7 +1,22 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        res = 0
-        for i in range(1, min(a, b) + 1):
-            if a % i == 0 and b % i == 0:
-                res += 1
+        if a < b:
+            return self.commonFactors(b, a)
+        while b != 0:
+            a, b = b, a % b
+        factor2num = dict()
+        factor = 2
+        while a != 1:
+            if a % factor == 0:
+                a /= factor
+                if factor not in factor2num:
+                    factor2num[factor] = 0
+                factor2num[factor] += 1
+            else:
+                factor += 1
+        res = 1
+        for k, v in factor2num.items():
+            res *= v + 1
         return res
+            
+            
