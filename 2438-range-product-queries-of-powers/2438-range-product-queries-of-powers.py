41277c47b40a6678f21e079ceeceffe11c8b1dbc
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-15 22:47:27 -0400

    Time: 1947 ms (100.00%), Space: 49.5 MB (100.00%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
@@ -1,25 +1,29 @@
 class Solution:
+    @staticmethod
+    @cache
+    def powerfor2(idx):
+        if idx == 0:
+            return 1
+        elif idx == 1:
+            return 2
+        else:
+            return (Solution.powerfor2(idx // 2) * Solution.powerfor2(idx - idx // 2)) % 1000000007
+    
+    @staticmethod
+    @cache
+    def find_less(n):
+        if n == 1:
+            return 0
+        else:
+            return 1 + Solution.find_less(n // 2)
+        
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:   
-        @cache
-        def powerfor2(idx):
-            if idx == 0:
-                return 1
-            elif idx == 1:
-                return 2
-            else:
-                return (powerfor2(idx // 2) * powerfor2(idx - idx // 2)) % 1000000007
-
-        @cache
-        def find_less(n):
-            if n == 1:
-                return 0
-            else:
-                return 1 + find_less(n // 2)
+        
             
         powers = []
         while n != 0:
-            powers.append(find_less(n))
-            n -= powerfor2(powers[-1])
+            powers.append(self.find_less(n))
+            n -= self.powerfor2(powers[-1])
         powers.sort()
         
         prefixS = [0]
@@ -28,7 +32,7 @@ class Solution:
         
         res = []
         for left, right in queries:
-            res.append(powerfor2(prefixS[right + 1] - prefixS[left]))
+            res.append(self.powerfor2(prefixS[right + 1] - prefixS[left]))
         return res
             
         
