3039feef6af25c24d5e3c73581843f9d52930b61
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-15 23:33:26 -0400

    Time: 1898 ms (60.00%), Space: 27 MB (100.00%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -2,9 +2,10 @@ class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         res = 0
         summa = 0
+        length = len(nums)
         for i, v in enumerate(nums):
             summa += v
-            if i == len(nums) - 1 or nums[i] > nums[i + 1]:
+            if i == length - 1 or nums[i] > nums[i + 1]:
                 res = max(res, (summa // (i + 1) 
                            if (summa % (i + 1) == 0) 
                            else (summa // (i + 1) + 1)))
