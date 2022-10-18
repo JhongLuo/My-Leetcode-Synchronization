f0cb61f144928e078a823b021e5bfb298c117fa5
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-18 15:44:47 -0400

    Time: 69 ms (71.78%), Space: 16 MB (18.12%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.py
+++ 1006-clumsy-factorial/1006-clumsy-factorial.py
@@ -2,16 +2,17 @@ class Solution:
     @cache
     @staticmethod
     def recur(n):
-        if n > 4:
-            return - (n * (n - 1) // (n - 2)) + (n - 3) + Solution.recur(n - 4)
-        elif n == 1:
+        if n == 0:
+            return 0
+        if n == 1:
             return -1
         elif n == 2:
             return -2
         elif n == 3:
             return -6
-        elif n == 4:
-            return -5
+        else:
+            return - (n * (n - 1) // (n - 2)) + (n - 3) + Solution.recur(n - 4)
+
             
     def clumsy(self, n: int) -> int:
         if n == 1:
