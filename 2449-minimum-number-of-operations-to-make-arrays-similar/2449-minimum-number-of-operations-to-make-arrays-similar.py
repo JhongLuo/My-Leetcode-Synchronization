4926e911822002317afd04d1c6b5b704cd656cee
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-23 20:31:46 -0400

    Time: 2710 ms (16.67%), Space: 32.6 MB (91.67%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.py
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.py
@@ -2,14 +2,14 @@ class Solution:
     def makeSimilar(self, nums: List[int], target: List[int]) -> int:
         nums.sort()
         target.sort()
-        even_nums = list(filter(lambda v: v%2 == 0, nums))
-        odd_nums = list(filter(lambda v: v%2 == 1, nums))
-        even_target = list(filter(lambda v: v%2 == 0, target))
-        odd_target = list(filter(lambda v: v%2 == 1, target))
+        even_nums = filter(lambda v: v%2 == 0, nums)
+        odd_nums = filter(lambda v: v%2 == 1, nums)
+        even_target = filter(lambda v: v%2 == 0, target)
+        odd_target = filter(lambda v: v%2 == 1, target)
         
         res = 0
-        for i in range(len(even_nums)):
-            res += abs(even_nums[i] - even_target[i])            
-        for i in range(len(odd_nums)):
-            res += abs(odd_nums[i] - odd_target[i])
+        for a, b in zip(even_nums, even_target):
+            res += abs(a - b)            
+        for a, b in zip(odd_nums, odd_target):
+            res += abs(a - b)
         return res // 4
