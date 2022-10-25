cd3e6aa825542d6e2c627c20fc9d74e5e41c2d9b
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-10-25 18:09:55 -0400

    Time: 1324 ms (73.11%), Space: 144.6 MB (14.06%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -5,39 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def recur(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        if root == None:
-            return False, False, ''
-        
-        start1, dest1, str1 = self.recur(root.left, startValue, destValue)
-        # if root.left != None:
-        #     print(root.left.val, start1, dest1, str1)
-        start2, dest2, str2 = self.recur(root.right, startValue, destValue)
-        # if root.right != None:
-        #     print(root.right.val, start2, dest2, str2)
-        
-        if start1 and dest1:
-            return start1, dest1, str1
-        elif start2 and dest2:
-            return start2, dest2, str2
-        elif start1 and dest2:
-            return start1, dest2, str1 + 'U' + 'R' + str2
-        elif start2 and dest1:    
-            return start2, dest1, str2 + 'U' + 'L' + str1
-        else:
-            rtnStart = start1 or start2 or root.val == startValue
-            rtnDest = dest1 or dest2 or root.val == destValue
-            if start1:
-                return rtnStart, rtnDest, str1 + 'U'
-            elif start2:
-                return rtnStart, rtnDest, str2 + 'U'
-            elif dest1:
-                return rtnStart, rtnDest, 'L' + str1
-            elif dest2:
-                return rtnStart, rtnDest, 'R' + str2
-            else:
-                return rtnStart, rtnDest, ''
-    
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        _, _, s = self.recur(root, startValue, destValue)
-        return s
+        son2father = dict()
+        self.startNode = None
+        def recur(father, node):
+            if node == None:
+                return
+            else:
+                if father != None:
+                    son2father[node] = father
+                if node.val == startValue:
+                    self.startNode = node
+                recur(node, node.left)
+                recur(node, node.right)
+                
+                
+        recur(None, root)
+        queue = [(self.startNode, '')]
+        visited = set()
+        while len(queue):
+            node, path = queue.pop()
+            visited.add(node)
+            if node.val == destValue:
+                return path
+            if node.left and node.left not in visited:
+                queue.append((node.left, path + 'L'))
+            if node.right and node.right not in visited:
+                queue.append((node.right, path + 'R'))
+            if node in son2father and son2father[node] not in visited:
+                queue.append((son2father[node], path + 'U'))
+        
+                
